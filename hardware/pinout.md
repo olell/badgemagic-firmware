0435532f0ace0cfb8762f9c1c9bd78a2e254a69d
olel <olel@olel.de>
2024-01-02 22:52:38 +0100

    Some more work on tracing / pinout + simple charlieplexing test

--- hardware/pinout.md
+++ hardware/pinout.md
@@ -4,45 +4,46 @@ Based on PCB marked with "B1144_221028"
 
 CH582M
 
-| No. | Pin      | Function    | Description |
-| --: | -------- | ----------- | ----------- |
-|   4 | **PA7**  |             |             |
-|   5 | **PA8**  |             |             |
-|   6 | **PA9**  |             |             |
-|  10 | **PB16** |             |             |
-|  29 | **PB19** |             |             |
-|  37 | **PA5**  |             |             |
-|  38 | **PA6**  |             |             |
-|  39 | **PA0**  |             |             |
-|  40 | **PA1**  |             |             |
-|  41 | **PA2**  |             |             |
-|  42 | **PA3**  |             |             |
-|  44 | **PA14** |             |             |
-|  45 | **PA13** |             |             |
-|  28 | **PB20** | Col 1       |             |
-|  27 | **PB21** | Col 2       |             |
-|  25 | **PB23** | Col 3       |             |
-|  23 | **PB1**  | Col 4       |             |
-|  22 | **PB2**  | Col 5       |             |
-|  20 | **PB4**  | Col 6       |             |
-|  21 | **PB3**  | Col 7       |             |
-|  36 | **PA4**  | Col 8       |             |
-|  19 | **PB5**  | Col 9       |             |
-|  14 | **PB12** | Col 10      |             |
-|  13 | **PB13** | Col 11      |             |
-|  12 | **PB14** | Col 12      |             |
-|  11 | **PB15** | Col 13      |             |
-|   8 | **PB8**  | Col 14      |             |
-|   7 | **PB9**  | Col 15      |             |
-|  47 | **PA11** | Col 16      |             |
-|  48 | **PA10** | Col 17      |             |
-|  46 | **PA12** | Col 18      |             |
-|  17 | **PB7**  | Col 19      |             |
-|  24 | **PB0**  | Col 20      |             |
-|  30 | **PB18** | Col 21      |             |
-|  43 | **PA15** | Col 22      |             |
-|  26 | **PB22** | Button      |             |
-|  15 | **PB11** | USB         |             |
-|  16 | **PB10** | USB         |             |
-|  18 | **PB6**  | NC?         |             |
-|   9 | **PB17** | PCB Pad TCK |             |
+|  No. | Pin       | Function                | Description  |
+| ---: | --------- | ----------------------- | ------------ |
+|   29 | **PB19**  | Charlieplex             |              |
+|   40 | **PA1**   | Charlieplex             |              |
+|   28 | **PB20**  | Charlieplex             |              |
+|   27 | **PB21**  | Charlieplex             |              |
+|   25 | **PB23**  | Charlieplex             |              |
+|   23 | **PB1**   | Charlieplex             |              |
+|   22 | **PB2**   | Charlieplex             |              |
+|   20 | **PB4**   | Charlieplex             |              |
+|   21 | **PB3**   | Charlieplex             |              |
+|   36 | **PA4**   | Charlieplex             |              |
+|   19 | **PB5**   | Charlieplex             |              |
+|   14 | **PB12**  | Charlieplex             |              |
+|   13 | **PB13**  | Charlieplex             |              |
+|   12 | **PB14**  | Charlieplex             |              |
+|   11 | **PB15**  | Charlieplex             |              |
+|    8 | **PB8**   | Charlieplex             |              |
+|    7 | **PB9**   | Charlieplex             |              |
+|   47 | **PA11**  | Charlieplex             |              |
+|   48 | **PA10**  | Charlieplex             |              |
+|   46 | **PA12**  | Charlieplex             |              |
+|   17 | **PB7**   | Charlieplex             |              |
+|   24 | **PB0**   | Charlieplex             |              |
+|   30 | **PB18**  | Charlieplex             |              |
+|   43 | **PA15**  | Charlieplex             |              |
+| ---- | --------- | ----------------------- | ------------ |
+|   39 | **PA0**   | LTH7 Pin 1              |              |
+|   26 | **PB22**  | Button                  |              |
+|   15 | **PB11**  | USB                     |              |
+|   16 | **PB10**  | USB                     |              |
+|    9 | **PB17**  | PCB Pad TCK             |              |
+|    5 | **PA8**   | PCB Pad RX              |              |
+|    6 | **PA9**   | PCB Pad TX              |              |
+|   10 | **PB16**  | PCB Pad TIO             |              |
+|   37 | **PA5**   | Battery Voltage Divider |              |
+|   41 | **PA2**   | Not known yet           |              |
+|   38 | **PA6**   | NC                      |              |
+|   42 | **PA3**   | NC                      |              |
+|   44 | **PA14**  | NC                      |              |
+|   45 | **PA13**  | NC                      |              |
+|   18 | **PB6**   | NC                      |              |
+|    4 | **PA7**   | NC                      |              |
--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
 #![no_std]
 #![no_main]
 
-use core::{array, panic::PanicInfo};
+use core::panic::PanicInfo;
 
 /// This function is called on panic.
 #[panic_handler]
@@ -10,138 +10,79 @@ fn panic(_info: &PanicInfo) -> ! {
 }
 
 extern crate ch58x_hal as hal;
-use hal::{
-    gpio::{Input, Level, Output, OutputDrive, Pull},
-    peripherals::PB22,
-};
+use hal::gpio::{AnyPin, Flex, Input, Level, Output, OutputDrive, Pin, Pull};
 
 extern crate embedded_hal;
-use embedded_hal::delay::DelayNs;
-use hal::delay::CycleDelay;
+
+const OUTPUT_DRIVE: OutputDrive = OutputDrive::_5mA;
 
 #[ch32v_rt::entry]
 fn main() -> ! {
     let mut config = hal::Config::default();
-    config.clock.use_pll_32mhz(); //.enable_lse();
+    config.clock.use_pll_32mhz();
+
     let p = hal::init(config);
 
     // switch
     let mut PB22 = Input::new(p.PB22, Pull::Up);
 
-    // Probably led pins
-    let mut PA7 = Output::new(p.PA7, Level::Low, OutputDrive::_5mA);
-    let mut PA8 = Output::new(p.PA8, Level::Low, OutputDrive::_5mA);
-    let mut PA9 = Output::new(p.PA9, Level::Low, OutputDrive::_5mA);
-    let mut PB9 = Output::new(p.PB9, Level::Low, OutputDrive::_5mA);
-    let mut PB8 = Output::new(p.PB8, Level::Low, OutputDrive::_5mA);
-    let mut PB16 = Output::new(p.PB16, Level::Low, OutputDrive::_5mA);
-    let mut PB15 = Output::new(p.PB15, Level::Low, OutputDrive::_5mA);
-    let mut PB14 = Output::new(p.PB14, Level::Low, OutputDrive::_5mA);
-    let mut PB13 = Output::new(p.PB13, Level::Low, OutputDrive::_5mA);
-    let mut PB12 = Output::new(p.PB12, Level::Low, OutputDrive::_5mA);
-    let mut PB7 = Output::new(p.PB7, Level::Low, OutputDrive::_5mA);
-    let mut PB5 = Output::new(p.PB5, Level::Low, OutputDrive::_5mA);
-    let mut PB4 = Output::new(p.PB4, Level::Low, OutputDrive::_5mA);
-    let mut PB3 = Output::new(p.PB3, Level::Low, OutputDrive::_5mA);
-    let mut PB2 = Output::new(p.PB2, Level::Low, OutputDrive::_5mA);
-    let mut PB1 = Output::new(p.PB1, Level::Low, OutputDrive::_5mA);
-    let mut PB0 = Output::new(p.PB0, Level::Low, OutputDrive::_5mA);
-    let mut PB23 = Output::new(p.PB23, Level::Low, OutputDrive::_5mA);
-    let mut PB21 = Output::new(p.PB21, Level::Low, OutputDrive::_5mA);
-    let mut PB20 = Output::new(p.PB20, Level::Low, OutputDrive::_5mA);
-    let mut PB19 = Output::new(p.PB19, Level::Low, OutputDrive::_5mA);
-    let mut PB18 = Output::new(p.PB18, Level::Low, OutputDrive::_5mA);
-    let mut PA4 = Output::new(p.PA4, Level::Low, OutputDrive::_5mA);
-    let mut PA5 = Output::new(p.PA5, Level::Low, OutputDrive::_5mA);
-    let mut PA6 = Output::new(p.PA6, Level::Low, OutputDrive::_5mA);
-    let mut PA0 = Output::new(p.PA0, Level::Low, OutputDrive::_5mA);
-    let mut PA1 = Output::new(p.PA1, Level::Low, OutputDrive::_5mA);
-    let mut PA2 = Output::new(p.PA2, Level::Low, OutputDrive::_5mA);
-    let mut PA3 = Output::new(p.PA3, Level::Low, OutputDrive::_5mA);
-    let mut PA15 = Output::new(p.PA15, Level::Low, OutputDrive::_5mA);
-    let mut PA14 = Output::new(p.PA14, Level::Low, OutputDrive::_5mA);
-    let mut PA13 = Output::new(p.PA13, Level::Low, OutputDrive::_5mA);
-    let mut PA12 = Output::new(p.PA12, Level::Low, OutputDrive::_5mA);
-    let mut PA11 = Output::new(p.PA11, Level::Low, OutputDrive::_5mA);
-    let mut PA10 = Output::new(p.PA10, Level::Low, OutputDrive::_5mA);
+    //&mut Output::new(p.PA8, Level::Low, OutputDrive::_5mA).degrade(),
+    //&mut Output::new(p.PA9, Level::Low, OutputDrive::_5mA).degrade(),
+    //&mut Output::new(p.PB16, Level::Low, OutputDrive::_5mA).degrade(),
 
-    macro_rules! wait {
-        () => {
-            while PB22.is_high() {
-                CycleDelay.delay_ms(1);
-            }
-            while PB22.is_low() {
-                CycleDelay.delay_ms(1);
-            }
-        };
+    // Probably led pins
+
+    let pins = [
+        // xx
+        &mut Flex::new(p.PB20).degrade(),
+        &mut Flex::new(p.PB21).degrade(),
+        &mut Flex::new(p.PB23).degrade(),
+        &mut Flex::new(p.PB1).degrade(),
+        &mut Flex::new(p.PB2).degrade(),
+        &mut Flex::new(p.PB4).degrade(),
+        &mut Flex::new(p.PB3).degrade(),
+        &mut Flex::new(p.PA4).degrade(),
+        &mut Flex::new(p.PB5).degrade(),
+        &mut Flex::new(p.PB12).degrade(),
+        &mut Flex::new(p.PB13).degrade(),
+        &mut Flex::new(p.PB14).degrade(),
+        &mut Flex::new(p.PB15).degrade(),
+        &mut Flex::new(p.PB8).degrade(),
+        &mut Flex::new(p.PB9).degrade(),
+        &mut Flex::new(p.PA11).degrade(),
+        &mut Flex::new(p.PA10).degrade(),
+        &mut Flex::new(p.PA12).degrade(),
+        &mut Flex::new(p.PB7).degrade(),
+        &mut Flex::new(p.PB0).degrade(),
+        &mut Flex::new(p.PB18).degrade(),
+        &mut Flex::new(p.PA15).degrade(),
+        // xx
+        &mut Flex::new(p.PB19).degrade(),
+        &mut Flex::new(p.PA1).degrade(),
+    ];
+
+    for i in 0..pins.len() {
+        pins[i].set_as_input(Pull::None);
     }
 
     loop {
-        PB20.set_high();
-        CycleDelay.delay_ms(10);
-        PB20.set_low();
-        PB21.set_high();
-        CycleDelay.delay_ms(10);
-        PB21.set_low();
-        PB23.set_high();
-        CycleDelay.delay_ms(10);
-        PB23.set_low();
-        PB1.set_high();
-        CycleDelay.delay_ms(10);
-        PB1.set_low();
-        PB2.set_high();
-        CycleDelay.delay_ms(10);
-        PB2.set_low();
-        PB4.set_high();
-        CycleDelay.delay_ms(10);
-        PB4.set_low();
-        PB3.set_high();
-        CycleDelay.delay_ms(10);
-        PB3.set_low();
-        PA4.set_high();
-        CycleDelay.delay_ms(10);
-        PA4.set_low();
-        PB5.set_high();
-        CycleDelay.delay_ms(10);
-        PB5.set_low();
-        PB12.set_high();
-        CycleDelay.delay_ms(10);
-        PB12.set_low();
-        PB13.set_high();
-        CycleDelay.delay_ms(10);
-        PB13.set_low();
-        PB14.set_high();
-        CycleDelay.delay_ms(10);
-        PB14.set_low();
-        PB15.set_high();
-        CycleDelay.delay_ms(10);
-        PB15.set_low();
-        PB8.set_high();
-        CycleDelay.delay_ms(10);
-        PB8.set_low();
-        PB9.set_high();
-        CycleDelay.delay_ms(10);
-        PB9.set_low();
-        PA11.set_high();
-        CycleDelay.delay_ms(10);
-        PA11.set_low();
-        PA10.set_high();
-        CycleDelay.delay_ms(10);
-        PA10.set_low();
-        PA12.set_high();
-        CycleDelay.delay_ms(10);
-        PA12.set_low();
-        PB7.set_high();
-        CycleDelay.delay_ms(10);
-        PB7.set_low();
-        PB0.set_high();
-        CycleDelay.delay_ms(10);
-        PB0.set_low();
-        PB18.set_high();
-        CycleDelay.delay_ms(10);
-        PB18.set_low();
-        PA15.set_high();
-        CycleDelay.delay_ms(10);
-        PA15.set_low();
+        for i in 0..pins.len() {
+            for j in 0..pins.len() {
+                if i != j {
+                    pins[i].set_as_output(OUTPUT_DRIVE);
+                    pins[j].set_as_output(OUTPUT_DRIVE);
+                    //hal::delay_us(10);
+                    pins[i].set_low();
+                    pins[j].set_high();
+                    //hal::delay_ms(5);
+                    pins[i].set_high();
+                    pins[j].set_low();
+                    //hal::delay_ms(5);
+                    pins[i].set_as_input(Pull::None);
+                    pins[j].set_as_input(Pull::None);
+                    //hal::delay_us(10);
+                }
+            }
+        }
     }
 }
